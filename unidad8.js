f776d196a2959b00d355db1a4a4d22a713889a66
Fragoi95 <goitiafrancisco09@gmail.com>
2023-08-11 08:58:43 +0200

    Se agregaron cambios en el archivo de javascript

---
+++ unidad8.js
@@ -0,0 +1,15 @@
+var caja = document.getElementById('contenido')
+
+caja.style.backgroundColor = '#ff0000'
+
+var parrafos = document.getElementsByTagName('p')
+
+parrafos[0].style.color = '#00FF00'
+parrafos[1].style.fontSize = '24px'
+parrafos[2].style.textAlign = 'center'
+
+var lista = document.getElementsByClassName('lista')
+
+lista[0].style.color = '#0000FF'
+lista[1].style.backgroundColor = 'pink'
+lista[2].style.fontWeight = 'bold'
